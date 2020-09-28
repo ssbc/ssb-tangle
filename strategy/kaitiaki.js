170397e7bec90cec24a199e8bb878273b8ed535e
chereseeriepa <cherese.eriepa@outlook.com>
2020-09-28 15:35:41 +1300

    update kaitiaki transformation and tests

--- strategy/kaitiaki.js
+++ strategy/kaitiaki.js
@@ -1,31 +1,27 @@
 const isEqual = require('lodash.isequal')
+const isEmpty = require('lodash.isempty')
+const { isFeed } = require('ssb-ref')
 
-const IDENTITY = {
-  add: [],
-  remove: []
-}
+const IDENTITY = {}
 
 function KaitiakiRule () {
-  // Entry = { feed: String, seq: Integer }
-  // transformations are of form:
-  // - { add: [], remove: []} (identity)
-  // - { add: [Entry], remove: [Entry] }
-  // - { add: [Entry], remove: [] }
-  // - { add: [], remove: [Entry] }
-  
+  /*
+    transformations are of form:  
+    {
+      String: { Integer: Integer }
+    }
+  */
 
   function isTransformation (T) {
     if (isIdentity(T)) return true
 
+    if (isEmpty(T)) return false
     if (T === null) return false
     if (typeof T !== 'object') return false
-    if (Object.keys(T).length !== 2) return false
-    
-    const { add, remove } = T
 
-    if (!isValidArray(add) || !isValidArray(remove)) return false
-  
-    return true
+    return Object.entries(T).every(([key, value]) => {
+      return isFeed(key) && isValidEntry(value)
+    })
   }
 
   function reify (T) {
@@ -46,12 +42,23 @@ function isIdentity (T) {
   return isEqual(T, IDENTITY)
 }
 
-function isValidArray (array) {
-  if (!array || !Array.isArray(array)) return false
+function isValidEntry (obj) {
+  if (isEmpty(obj)) return false
+  if (obj === null || obj === undefined) return false
+  if (typeof obj !== 'object') return false
 
-  return !array.some(d => {
-    return !(d.feed && d.seq && typeof d.feed === 'string' && Number.isInteger(d.seq))
+  return Object.entries(obj).every(([key, value]) => {
+    return isValidKey(key) && isValidValue(value)
   })
+}
+
+function isValidKey (key) {
+  if (typeof key === 'string') key = parseInt(key)
+  return Number.isInteger(key) && key > 0
+}
+
+function isValidValue (value) {
+  return Number.isInteger(value)
 }
 
 module.exports = KaitiakiRule
--- test/strategy/katiaki.test.js
+++ test/strategy/katiaki.test.js
@@ -11,51 +11,103 @@ test.only('strategy/kaitiaki', t => {
 
   // isTransformation /////////////////////////////
 
-  const a = { 
-    add: [{ feed: '@ben', seq: 200 }, { feed: '@cherese', seq: 500 }], 
-    remove: [] 
-  }
-  
-  const b = { 
-    add: [], 
-    remove: [{ feed: '@ben', seq: 500 }] 
-  }
-
   const Ts = [
-    a,
-    b,
-    identity() // { add: [], remove: [] }
+    { // simple add
+      '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        100: 1
+      }
+    },
+    { // simple remove
+      '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        500: -1
+      }
+    },
+    { // multiple adds in a row
+      '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        100: 1, // cherese added at seq 100 by mix
+        300: 1  // added again at seq 300 later on by ben
+      }
+    },
+    { // muliple feeds different seq
+      '@mixVv6k5mnWKkJTgCIpqOsA4JeJd9Oz2gmv6rojQeXU=.ed25519': {
+        100: 1 // mix added at seq 100
+      },
+      '@bengCIpqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        150: 1 // ben added after mix at seq 150
+      }
+    },
+    { // mutiple feeds same seq
+      '@mixVv6k5mnWKkJTgCIpqOsA4JeJd9Oz2gmv6rojQeXU=.ed25519': {
+        100: 1
+      },
+      '@bengCIpqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        100: 1
+      }
+    },
+    { // multiple removes in a row
+      '@bengCIpqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        200: 1, // ben added at seq 200
+        550: -1, // remove at seq 550 (by cherese)
+        500: -1 // removed at seq 500 (by mix who was out of the loop)
+      }
+    },
+    {
+      '@bengCIpqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        100: 1,
+        200: 2
+      }
+    },
+    {
+      '@bengCIpqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        100: 2,
+        200: 1
+      }
+    },
+    {
+      '@bengCIpqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        500: 1,
+        100: 2
+      }
+    },
+    identity() // {}
   ]
 
   Ts.forEach(T => {
     t.true(isTransformation(T), `isTransformation : ${JSON.stringify(T)}`)
   })
 
-  var testEntry = (feed, seq) => ({ feed, seq })
-
   const notTs = [
-    'dog',
-    { content: 'dog' },
-    undefined,
+    [],
     null,
-    { set: undefined },
-    { add: [], other: [] },
-    { other: [], remove: [] },
-    { add: null, remove: null },
-    { add: undefined, remove: undefined },
-    { add: '', remove: '' },
-    { add: false, remove: false },
-    { add: true, remove: true },
-    { add: [], remove: null },
-    { add: null, remove: [] },
-    
-    // array item
-    { add: [testEntry(null, null)], remove: [] },
-    { add: [testEntry(undefined, undefined)], remove: [] },
-    { add: [testEntry(false, false)], remove: [] },
-    { add: [testEntry(true, true)], remove: [] },
-    { add: [testEntry('@ben', 200), testEntry('@mix', 400), testEntry(200, 200)], remove: [] },
-    { add: [testEntry(500, '@cherese')]},
+    undefined,
+    '',
+    'dog',
+    100,
+    {
+      '@mixVv6k5mnWKkJTgCIpqOsA4JeJd9Oz2gmv6rojQeXU=.ed25519': {
+        'seq': 1 // invalid LHS expected to be int
+      }
+    },
+    {
+      '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        200: '2' // invalid RHS expected to be in
+      }
+    },
+    {
+      '@bengCIpqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {} // should this be allowed...?
+    },
+    {
+      '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': {
+        'invalid': 'invalid' // both sides invalid
+      }
+    },
+    {
+      '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': null
+    },
+    { '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': undefined },
+    { '@chereseqOsA4JeJVv6k5mnWKkJTd9Oz2gmv6rojQeXU=.ed25519': 123 },
+    { 123: { 200: 1 }}, // invalid feedId (number)
+    { '@cherese': { 200: 1 }} // invalid feedId (not a feedId)
   ]
   notTs.forEach(T => {
     t.false(isTransformation(T), `!isTransformation : ${JSON.stringify(T)}`)
